53bdf1814b75575e2bf1c8bccdb25913c5047382
Giancarlo Squitieri <giancarlosquitieri@gmail.com>
2024-05-18 22:02:12 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/container-queries')
+
+  ],
+  darkMode: "class"
+}
+
